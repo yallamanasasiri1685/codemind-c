067d9b4cc6da32e48812944d7d1b0ac21528b8ca
yallamanasasiri1685 <119730249+yallamanasasiri1685@users.noreply.github.com>
2022-12-03 21:40:32 +0530

    #include<stdio.h> int main() {     int ti,i;     scanf("%d",&ti);     for(i=1;i<=ti;i++)     {         int n,r,s=0,q;         scanf("%d",&n);         q=n;         while(q!=0)         {             r=q%10;             q=q/10;             s=s*10+r;         }         if(s==n)         {             printf("True ");         }         else         {             printf("False ");         }     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int ti,i;
+    scanf("%d",&ti);
+    for(i=1;i<=ti;i++)
+    {
+        int n,r,s=0,q;
+        scanf("%d",&n);
+        q=n;
+        while(q!=0)
+        {
+            r=q%10;
+            q=q/10;
+            s=s*10+r;
+        }
+        if(s==n)
+        {
+            printf("True
+");
+        }
+        else
+        {
+            printf("False
+");
+        }
+    }
+}
