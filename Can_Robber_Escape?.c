fcb6268fca8729af4d1488cc05536f421e0541c9
yallamanasasiri1685 <119730249+yallamanasasiri1685@users.noreply.github.com>
2023-05-24 20:15:15 +0530

    #include<stdio.h> int main(){     int n,l[100],c=0,f=1;     scanf("%d",&n);     for(int i=0;i<n;i++){         c+=1;         scanf("%d",&l[i]);     }     for(int j=0;j<=n;j++){         if(l[j]>=n){             f=0;             break;         }     }     if(f==1)     printf("YES");     else     printf("NO"); }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,27 +1,19 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int n,l[100],c=0,f=1;
     scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         c+=1;
         scanf("%d",&l[i]);
     }
-    for(int j=0;j<n;j++)
-    {
-        if(l[j]>=n)
-        {
+    for(int j=0;j<=n;j++){
+        if(l[j]>=n){
             f=0;
             break;
         }
     }
     if(f==1)
-    {
-        printf("YES");
-    }
+    printf("YES");
     else
-    {
-        printf("NO");
-    }
+    printf("NO");
 }
