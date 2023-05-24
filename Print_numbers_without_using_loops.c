f9e430c36c7f6e955c07809e604b0e164b5c4332
yallamanasasiri1685 <119730249+yallamanasasiri1685@users.noreply.github.com>
2023-05-24 21:48:09 +0530

    #include <stdio.h> void pn(int s,int k) {     if(k>s)     return;     printf("%d ",k);     pn(s,++k);
    }
    int main()
    {
        int s,k=1;
        scanf("%d",&s);
        if(s>=1 && s<=10000)
        {
            pn(s,k);
        }
        else
        {
            printf("The Number Series is Not Possible Print");
        }
        
    }

---
+++ Print_numbers_without_using_loops.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void pn(int s,int k)
+{
+    if(k>s)
+    return;
+    printf("%d ",k);
+    pn(s,++k);
+    
+}
+int main()
+{
+    int s,k=1;
+    scanf("%d",&s);
+    if(s>=1 && s<=10000)
+    {
+        pn(s,k);
+    }
+    else
+    {
+        printf("The Number Series is Not Possible Print");
+    }
+    
+}
