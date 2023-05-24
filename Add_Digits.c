b23a3e548dfe72b73ba4387bed2cf5d67d37400f
yallamanasasiri1685 <119730249+yallamanasasiri1685@users.noreply.github.com>
2023-05-24 13:49:58 +0530

    #include<stdio.h> int main() {     int n,i,j,r,s=0,sl=0,rl;     scanf("%d",&n);     i=n-(n-1)/9*9;     printf("%d",i); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,s=0,r;
+    int n,i,j,r,s=0,sl=0,rl;
     scanf("%d",&n);
-    while(n/10!=0)
-    {
-        s=0;
-        while(n!=0)
-        {
-            m=n%10;
-            s=s+m;
-            n=n/10;
-        }
-        n=s;
-    }
-    printf("%d",s);
+    i=n-(n-1)/9*9;
+    printf("%d",i);
 }
